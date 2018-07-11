6f943f25178cebcd0cdd1bf1d46ea31302cc24a9
Dragos Sabau <sabaudragos@yahoo.com>
2018-07-11 21:52:22 +0300

    Enabled zuul proxy and excepted the Authorization header from sensitive headers list

--- src/main/java/xyz/vegaone/gateway/GatewayApplication.java
+++ src/main/java/xyz/vegaone/gateway/GatewayApplication.java
@@ -2,11 +2,13 @@ package xyz.vegaone.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
